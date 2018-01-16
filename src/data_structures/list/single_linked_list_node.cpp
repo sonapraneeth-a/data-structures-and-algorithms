bcbfc7b59ec2af2abc3ef175be41223a1bd5c4ea
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-01-16 23:37:50 +0530

    Added code for Single Linked List Node

---
+++ src/data_structures/list/single_linked_list_node.cpp
@@ -0,0 +1,6 @@
+#include "single_linked_list_node.h"
+
+int print_list_value()
+{
+    return 100;
+}
---
+++ src/data_structures/list/single_linked_list_node.h
@@ -0,0 +1,6 @@
+#ifndef LIST_SLL_NODE_H
+#define LIST_SLL_NODE_H
+
+int print_list_value();
+
+#endif
