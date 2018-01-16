962f3f6453cd1d7daffc98cbb0b502b9d43ad12a
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-01-16 23:37:21 +0530

    Added code for main and CMakeLists.txt for compiling high level program

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "single_linked_list_node.h"
 
 int main(int argc, char const *argv[])
 {
     std::cout << "Hello, Data Structures and Algorithms" << std::endl;
+    std::cout << print_list_value() << std::endl;
     return 0;
 }
