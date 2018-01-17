56823ec31f19647065b8d1cfa8481ae6b98a9cea
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-01-17 16:06:57 +0530

    Added code for single linked list node

--- src/data_structures/list/single_linked_list_node.cpp
+++ src/data_structures/list/single_linked_list_node.cpp
@@ -1,6 +1,51 @@
 #include "single_linked_list_node.h"
 
-int print_list_value()
+template <typename T>
+single_linked_list_node<T>::single_linked_list_node(T value)
 {
-    return 100;
+    data = value;
+    next = nullptr;
 }
+
+template <typename T>
+T single_linked_list_node<T>::get_data()
+{
+    return this->data;
+}
+
+template <typename T>
+single_linked_list_node<T>* single_linked_list_node<T>::get_next()
+{
+    return this->next;
+}
+
+template <typename T>
+void single_linked_list_node<T>::set_data(T value)
+{
+    data = value;
+}
+
+template <typename T>
+void single_linked_list_node<T>::set_next(single_linked_list_node<T> *next_link)
+{
+    next = next_link;
+}
+
+template <typename T>
+std::string single_linked_list_node<T>::to_string()
+{
+    std::stringstream out;
+    out << this->data << " -> " << this->next;
+    return out.str();
+}
+
+template <typename T>
+single_linked_list_node<T>::~single_linked_list_node()
+{
+    next = nullptr;
+}
+
+template class single_linked_list_node<int>;
+template class single_linked_list_node<double>;
+template class single_linked_list_node<char>;
+template class single_linked_list_node<std::string>;
--- src/data_structures/list/single_linked_list_node.h
+++ src/data_structures/list/single_linked_list_node.h
@@ -1,6 +1,34 @@
-#ifndef LIST_SLL_NODE_H
-#define LIST_SLL_NODE_H
+#ifndef DS_LIST_SLL_NODE_H
+#define DS_LIST_SLL_NODE_H
 
-int print_list_value();
+#include <iostream>
+#include <sstream>
+
+template<typename T>
+class single_linked_list_node
+{
+public:
+    T data;
+    single_linked_list_node<T> *next;
+
+    single_linked_list_node();
+    single_linked_list_node(T value);
+
+    T get_data();
+    single_linked_list_node<T>* get_next();
+    void set_data(T value);
+    void set_next(single_linked_list_node<T> *next_link);
+
+    std::string to_string();
+
+    ~single_linked_list_node();
+};
+
+template<typename T>
+std::ostream& operator <<(std::ostream& os, const single_linked_list_node<T>& node)
+{
+    os << node.data << " -> " << node.next << "\n";
+    return os;
+}
 
 #endif
