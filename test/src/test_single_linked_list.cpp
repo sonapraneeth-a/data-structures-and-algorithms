3de2a129eed6db21d228075092bcfb552cbbb75b
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-01-17 17:34:30 +0530

    Modified the test for single linked list to take advantage of google test fixtures

--- test/src/test_single_linked_list.cpp
+++ test/src/test_single_linked_list.cpp
@@ -4,29 +4,43 @@ using namespace std;
 
 class SingleLinkedListTest : public ::testing::Test
 {
+protected:
+    virtual void SetUp()
+    {
+        _list_int_1.insert(10);
+        _list_int_2.insert(10);
+        _list_int_2.insert(20);
+    }
+
+    /*virtual void TearDown()
+    {
+
+    }*/
+
+    // List with size 0 initially
+    single_linked_list<int> _list_int_0;
+    // List with size 1 initially
+    single_linked_list<int> _list_int_1;
+    // List with size 2 initially
+    single_linked_list<int> _list_int_2;
 
 };
 
-
 TEST_F(SingleLinkedListTest, InitHead)
 {
-    single_linked_list<int> list(15);
-    EXPECT_EQ(15, list.get_head()->get_data());
+    single_linked_list<int> list(10);
+    EXPECT_EQ(10, list.get_head()->get_data());
 }
 
 TEST_F(SingleLinkedListTest, InsertHeadValue)
 {
-    single_linked_list<int> list;
-    list.insert(20);
-    EXPECT_EQ(20, list.get_head()->get_data());
+    EXPECT_EQ(10, _list_int_1.get_head()->get_data());
 }
 
 TEST_F(SingleLinkedListTest, PrintList)
 {
-    single_linked_list<int> list;
-    list.insert(20);
-    list.insert(15);
-    std::string obtained_answer = list.to_string();
-    std::string expected_answer = "20 -> 15 -> 0";
-    EXPECT_EQ(expected_answer, expected_answer);
+    std::string obtained_answer = _list_int_2.to_string();
+    std::string expected_answer = "10 -> 20 -> 0";
+    EXPECT_EQ(expected_answer, obtained_answer);
 }
+
