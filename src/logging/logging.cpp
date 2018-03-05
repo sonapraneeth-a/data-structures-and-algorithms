49029e9974a42b5db98f57992204b7f4ea4910a6
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-05 11:49:07 +0530

    Added empty code for logging library

---
+++ src/logging/logging.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pranas on 1/24/2018.
+//
+
+#include "logging.h"
---
+++ src/logging/logging.h
@@ -0,0 +1,14 @@
+//
+// Created by pranas on 1/24/2018.
+//
+
+#ifndef DATA_STRUCTURES_AND_ALGORITHMS_LOGGING_H
+#define DATA_STRUCTURES_AND_ALGORITHMS_LOGGING_H
+
+
+class logging {
+
+};
+
+
+#endif //DATA_STRUCTURES_AND_ALGORITHMS_LOGGING_H
