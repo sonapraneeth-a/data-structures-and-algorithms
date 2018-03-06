6fb31a15664417c679f67acbbf2c8b23a627e166
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-06 19:04:20 +0530

    Updated main code for MainNumbers for specifically running specific test code locally

--- Src/Algorithms/Numbers/MainNumbers.cpp
+++ Src/Algorithms/Numbers/MainNumbers.cpp
@@ -1,9 +1,22 @@
 #include "BigInteger.h"
+#include "Factorization.h"
 
-int main()
+void RunBigInteger()
 {
     BigInteger number;
     std::string positive_signInteger = "+123";
     std::cout << number.validate_string(positive_signInteger) << "\n";
+    return;
+}
+
+void RunFactorization()
+{
+    std::cout << "GCD of 357 and 234 is: " << EuclideanGCD(357, 234) << "\n";
+    return;
+}
+
+int main()
+{
+    RunFactorization();
     return 0;
 }
