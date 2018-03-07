c2e0d777ab221900144aca0e22e6f76aac100d27
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-07 12:11:10 +0530

    Added definitions for long long int and updated timing code with new units

--- Src/Headers/DefaultHeaders.h
+++ Src/Headers/DefaultHeaders.h
@@ -6,6 +6,8 @@
 #include <ctime>
 #include <chrono>
 
+#define ULLI unsigned long long int
+#define LLI long long int
 
 #define EXECUTE_FUNC(func) \
     {\
@@ -13,22 +15,29 @@
         func\
         std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
-        std::cout << "Duration: " << duration << " ms\n";\
+        std::cout << "Duration: " << duration << " us\n";\
     }
 
-#define EXECUTE_FUNC_UNIT(unit, func) \
+#define EXECUTE_FUNC_UNIT(unit, code_block) \
     {\
         std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
-        func\
+        code_block\
         std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
         auto duration = 0;\
         if (unit == "ns")\
             duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();\
         else if (unit == "s")\
             duration = std::chrono::duration_cast<std::chrono::seconds>(t2-t1).count();\
+        else if (unit == "ms")\
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();\
+        else if (unit == "us")\
+            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
         else\
             duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
-        std::cout << "Duration: " << duration << " " << unit << "\n";\
+        if(!(unit == "ns" || unit == "ms" || unit == "us" || unit == "s"))\
+            std::cout << "Duration: " << duration << " us\n";\
+        else\
+            std::cout << "Duration: " << duration << " " << unit << "\n";\
     }
 
 #endif
