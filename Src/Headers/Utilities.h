320e01f8d383b2e9d0d35a5d4866171238389cd5
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-15 01:29:46 +0530

    Added CMakeLists for Headers

--- Src/Headers/Utilities.h
+++ Src/Headers/Utilities.h
@@ -4,30 +4,10 @@
 #include <random>
 #include <ctime>
 
-int GetRandomInt(int start=100, int end=100000, std::string mode="Fixed")
-{
-    int number;
-    if (mode == "Random")
-    {
-        srand((int) time(0));
-        number = (rand() % end) + start;
-    }
-    else
-    {
-        std::random_device rd;
-        std::mt19937 mt(rd());
-        std::uniform_int_distribution<int> dist(start, end);
-        number = dist(mt);
-    }
-    return number;
-}
+int
+GetRandomInt(int start=100, int end=100000, std::string mode="Fixed");
 
-double GetRandomDouble(int start=100, int end=100000, std::string mode="Fixed")
-{
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_real_distribution<double> dist(start, end);
-    return dist(mt);
-}
+double
+GetRandomDouble(int start=100, int end=100000, std::string mode="Fixed");
 
 #endif
