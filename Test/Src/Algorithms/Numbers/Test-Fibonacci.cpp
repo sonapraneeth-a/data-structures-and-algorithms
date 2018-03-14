4510df085a6ff21abb01d22a53a6cc984ae2dfbc
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-14 22:58:57 +0530

    Updated test cases for Fibonacci Series.

--- Test/Src/Algorithms/Numbers/Test-Fibonacci.cpp
+++ Test/Src/Algorithms/Numbers/Test-Fibonacci.cpp
@@ -1,14 +1,34 @@
 #include "Fibonacci.h"
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 class FibonacciTest : public ::testing::Test
 {
 protected:
     virtual void SetUp()
-    {}
+    {
+        fibonacciLength_01.push_back(0);
+
+        fibonacciLength_02.push_back(0);
+        fibonacciLength_02.push_back(1);
+
+        fibonacciLength_05.push_back(0);
+        fibonacciLength_05.push_back(1);
+        fibonacciLength_05.push_back(1);
+        fibonacciLength_05.push_back(2);
+        fibonacciLength_05.push_back(3);
+    }
 
     virtual void TearDown()
-    {}
+    {
+        fibonacciLength_01.clear();
+        fibonacciLength_02.clear();
+        fibonacciLength_05.clear();
+    }
+
+    std::vector<ULLI> fibonacciLength_01;
+    std::vector<ULLI> fibonacciLength_02;
+    std::vector<ULLI> fibonacciLength_05;
 
 };
 
@@ -17,6 +37,7 @@ TEST_F(FibonacciTest, SimpleIndexTestRecursive)
     EXPECT_EQ(0, GetNthFibonacciRecursive(0));
     EXPECT_EQ(1, GetNthFibonacciRecursive(1));
     EXPECT_EQ(1, GetNthFibonacciRecursive(2));
+    EXPECT_EQ(34, GetNthFibonacciRecursive(8));
 }
 
 TEST_F(FibonacciTest, SimpleIndexTestIterative)
@@ -24,5 +45,15 @@ TEST_F(FibonacciTest, SimpleIndexTestIterative)
     EXPECT_EQ(0, GetNthFibonacciIterative(0));
     EXPECT_EQ(1, GetNthFibonacciIterative(1));
     EXPECT_EQ(1, GetNthFibonacciIterative(2));
+    EXPECT_EQ(34, GetNthFibonacciIterative(8));
 }
 
+TEST_F(FibonacciTest, SeriesTest)
+{
+    EXPECT_THAT(fibonacciLength_01,
+                ::testing::ContainerEq(FibonacciSeries(1)));
+    EXPECT_THAT(fibonacciLength_02,
+                ::testing::ContainerEq(FibonacciSeries(2)));
+    EXPECT_THAT(fibonacciLength_05,
+                ::testing::ContainerEq(FibonacciSeries(5)));
+}
