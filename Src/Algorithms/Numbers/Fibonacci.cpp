8ae1c260718b8b61f4f2aa29fdb91ad433aa6c38
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-14 22:58:30 +0530

    Fixed a bug in calculating nth fibonacci number iterative version. Completed fibonacci series function

--- Src/Algorithms/Numbers/Fibonacci.cpp
+++ Src/Algorithms/Numbers/Fibonacci.cpp
@@ -66,10 +66,10 @@ GetNthFibonacciIterative(unsigned int index)
     }
     else
     {
-        unsigned long long int fn = 0;     // F(n)
         unsigned long long int fn_2 = 0;   // F(n-2)
         unsigned long long int fn_1 = 1;   // F(n-1)
-        for(unsigned int i = index; i <= index; i++)
+        unsigned long long int fn = 0;     // F(n)
+        for(unsigned int i = 2; i <= index; i++)
         {
             fn = fn_1 + fn_2; // Recursive relation: F(n) = F(n-1) + F(n-2)
             fn_2 = fn_1;
@@ -80,7 +80,6 @@ GetNthFibonacciIterative(unsigned int index)
 }
 
 /*!
- * @todo Complete the implementation of the function
  * @ingroup Fibonacci
  * @brief   Gets the fibonacci series of specified length
  * @details
@@ -92,4 +91,20 @@ std::vector<ULLI>
 FibonacciSeries(unsigned int length)
 {
     // Series: 0 1 1 2 3 5 ...
+    std::vector<ULLI> series;
+    if(length >= 1)
+    {
+        series.push_back(0);
+    }
+    if(length >= 2)
+    {
+        series.push_back(1);
+    }
+    // Recursive relation: F(n) = F(n-1) + F(n-2)
+    for (unsigned int index = 2; index < length; index++)
+    {
+        // Using the last two elements of series vector for the current series
+        series.push_back(series[index-1] + series[index-2]);
+    }
+    return series;
 }
