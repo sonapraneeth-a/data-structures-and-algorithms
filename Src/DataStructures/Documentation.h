0cafd49c3d6e22d7c980088feaa842b47991b1e6
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-04-02 19:37:55 +0530

    Updated Documentation header for DataStructures

---
+++ Src/DataStructures/Documentation.h
@@ -0,0 +1,42 @@
+/**
+ *      Created on: 01 April 2018
+ *   Last modified: 01 April 2018
+ *          Author: Sona Praneeth Akula
+ *         Details: Documentation for Data Structures
+ */
+
+#ifndef DATA_STRUCTURES_DOCUMENTATION_H
+#define DATA_STRUCTURES_DOCUMENTATION_H
+
+/**
+ * @defgroup Data structures
+ * @brief    Code for implementation of various data structures
+ */
+
+/**
+ * @defgroup Array
+ * @ingroup  Data structures
+ * @brief    Data structures related to arrays
+ */
+
+/**
+ * @defgroup List
+ * @ingroup  Data structures
+ * @brief    Data structures related to lists
+ */
+
+/**
+ * @defgroup Stack
+ * @ingroup  Data structures
+ * @brief    Data structures related to stacks
+ */
+
+/**
+ * @defgroup Queue
+ * @ingroup  Data structures
+ * @brief    Data structures related to queues
+ */
+
+
+
+#endif // DATA_STRUCTURES_DOCUMENTATION_H
