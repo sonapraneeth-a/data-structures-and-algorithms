5e43639c97b89d54878d1e36fd01937bb01bfbe2
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-04-02 19:33:37 +0530

    Added SampleCode for Queue(Not Tested)

--- Src/DataStructures/Queue/Queue.h
+++ Src/DataStructures/Queue/Queue.h
@@ -1,14 +1,42 @@
-//
-// Created by pranas on 3/20/2018.
-//
+/**
+ *      Created on: 20 March 2018
+ *   Last modified: 23 March 2018
+ *          Author: Sona Praneeth Akula
+ *         Details: Header for queue
+ */
 
-#ifndef DATASTRUCTURESANDALGORITHMS_STACK_H
-#define DATASTRUCTURESANDALGORITHMS_STACK_H
+/**
+ * Changelog
+ *
+ * Date (DD-MM-YYYY)            Author               Update
+ * 20-03-2018             Sona Praneeth Akula   - Creation of the file
+ * 23-03-2018             Sona Praneeth Akula   - Added function signatures
+ */
 
+#ifndef QUEUE_H
+#define QUEUE_H
 
-class Stack {
+#include "QueueNode.h"
 
+template <typename T>
+class Queue
+{
+public:
+
+    QueueNode<T> *Head;
+    QueueNode<T> *Tail;
+    ULLI Size = 0;
+    ULLI MaxSize = 0;
+
+    Queue();
+    Queue(T Value);
+
+    void Enqueue(T Value);
+    T Dequeue();
+    bool IsEmpty();
+
+    ~Queue();
 };
 
 
-#endif //DATASTRUCTURESANDALGORITHMS_STACK_H
+#endif //QUEUE_H
---
+++ Src/DataStructures/Queue/QueueNode.h
@@ -0,0 +1,45 @@
+/**
+ *      Created on: 23 March 2018
+ *   Last modified: 23 March 2018
+ *          Author: Sona Praneeth Akula
+ *         Details: Header for queue node
+ */
+
+/**
+ * Changelog
+ *
+ * Date (DD-MM-YYYY)            Author               Update
+ * 23-03-2018             Sona Praneeth Akula   - Creation of the file
+ *                                              - Added function signatures
+ */
+
+#ifndef QUEUE_NODE_H
+#define QUEUE_NODE_H
+
+#include "DefaultHeaders.h"
+#include <iostream>
+#include <sstream>
+
+template <typename T>
+class QueueNode
+{
+public:
+    T Data;
+    QueueNode<T> *Next;
+
+    QueueNode();
+    QueueNode(T Value);
+
+    T GetData();
+    QueueNode<T>* GetNext();
+    void SetData(T Value);
+    void SetNext(QueueNode<T> *NextLink);
+
+    std::string ToString();
+
+    ~QueueNode();
+
+};
+
+
+#endif //QUEUE_NODE_H
