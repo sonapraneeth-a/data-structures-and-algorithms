dfe78f47b7b568b1864855224b91322fa041d150
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-04-02 19:29:23 +0530

    Empty Search and Sort Header and Implementation files

---
+++ Src/Algorithms/SearchSort/Search.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pranas on 3/6/2018.
+//
+
+#include "Search.h"
---
+++ Src/Algorithms/SearchSort/Search.h
@@ -0,0 +1,14 @@
+//
+// Created by pranas on 3/6/2018.
+//
+
+#ifndef DATASTRUCTURESANDALGORITHMS_SEARCH_H
+#define DATASTRUCTURESANDALGORITHMS_SEARCH_H
+
+
+class Search {
+
+};
+
+
+#endif //DATASTRUCTURESANDALGORITHMS_SEARCH_H
