a1f651bddb33fa5831df91d6b3068a4ce309e29c
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-07-04 01:24:03 +0530

    Fixed an issue in path export

--- Scripts/Unix-Run.sh
+++ Scripts/Unix-Run.sh
@@ -7,5 +7,5 @@ mkdir -p Bin
 echo "Compiling the code by linking static libraries"
 g++ -std=c++11 -I./Include -L./Lib/ Main.cpp -o ./Bin/MainStatic.out -lDSA_Static_debug
 echo "Compiling the code by linking shared libraries"
-export LB_LIBRARY_PATH=./Lib/
+export LD_LIBRARY_PATH=./Lib/
 g++ -std=c++11 -I./Include -L./Lib/ Main.cpp -o ./Bin/MainShared.out -lDSA_Shared_debug
