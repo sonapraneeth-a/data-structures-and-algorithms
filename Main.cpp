41e233a59df5e951929d1682919577f4136f5312
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-07-04 23:22:57 +0530

    Added a sample main file to test the source code

--- Main.cpp
+++ Main.cpp
@@ -4,8 +4,13 @@
 
 #include "DefaultHeaders.h"
 #include "Factorization.h"
+#include "Stack.h"
 
 int main()
 {
     std::cout << EuclideanGCDIterative(6, 12) << "\n";
+    Stack<std::string> TestStack;
+    TestStack.Push("Rama");
+    std::cout << TestStack.Peek() << "\n";
+    return 0;
 }
