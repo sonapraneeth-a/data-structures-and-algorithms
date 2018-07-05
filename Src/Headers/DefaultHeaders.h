df02fffea304187ebc2d7aff342c28f50228ac69
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-07-05 19:51:19 +0530

    Added some minor changes to default and utilities header files

--- Src/Headers/DefaultHeaders.h
+++ Src/Headers/DefaultHeaders.h
@@ -25,40 +25,4 @@
 #define ULLI unsigned long long int
 #define LLI long long int
 
-#define MAJOR_VERSION 0
-#define MINOR_VERSION 3
-#define PATCH_VERSION 0
-
-
-#define EXECUTE_FUNC(func) \
-    {\
-        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
-        func\
-        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
-        std::cout << "Duration: " << duration << " us\n";\
-    }
-
-#define EXECUTE_FUNC_UNIT(unit, code_block) \
-    {\
-        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
-        code_block\
-        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
-        auto duration = 0;\
-        if (unit == "ns")\
-            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();\
-        else if (unit == "s")\
-            duration = std::chrono::duration_cast<std::chrono::seconds>(t2-t1).count();\
-        else if (unit == "ms")\
-            duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();\
-        else if (unit == "us")\
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
-        else\
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
-        if(!(unit == "ns" || unit == "ms" || unit == "us" || unit == "s"))\
-            std::cout << "Duration: " << duration << " us\n";\
-        else\
-            std::cout << "Duration: " << duration << " " << unit << "\n";\
-    }
-
 #endif
--- Src/Headers/Utilities.h
+++ Src/Headers/Utilities.h
@@ -5,7 +5,40 @@
 #include <ctime>
 #include "DefaultHeaders.h"
 
-#define LLI long long int
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 3
+#define PATCH_VERSION 0
+
+#define EXECUTE_FUNC(func) \
+    {\
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
+        func\
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
+        std::cout << "Duration: " << duration << " us\n";\
+    }
+
+#define EXECUTE_FUNC_UNIT(unit, code_block) \
+    {\
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
+        code_block\
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
+        auto duration = 0;\
+        if (unit == "ns")\
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();\
+        else if (unit == "s")\
+            duration = std::chrono::duration_cast<std::chrono::seconds>(t2-t1).count();\
+        else if (unit == "ms")\
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();\
+        else if (unit == "us")\
+            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
+        else\
+            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
+        if(!(unit == "ns" || unit == "ms" || unit == "us" || unit == "s"))\
+            std::cout << "Duration: " << duration << " us\n";\
+        else\
+            std::cout << "Duration: " << duration << " " << unit << "\n";\
+    }
 
 inline int
 GetRandomInt(int start=100, int end=100000, std::string mode="Fixed")
@@ -49,6 +82,18 @@ bool GreaterComparator(T first, T second)
 }
 
 template <typename T>
+bool StrictLessComparator(T first, T second)
+{
+    return first < second;
+}
+
+template <typename T>
+bool StrictGreaterComparator(T first, T second)
+{
+    return first > second;
+}
+
+template <typename T>
 void Swap(T& first, T& second)
 {
     T temp = second;
@@ -64,6 +109,12 @@ void PrintOneDArray(T Array[], LLI start, LLI end)
         std::cout << Array[index] << " ";
     }
     std::cout << "\n";
+}
+
+void
+PrintVersion()
+{
+    std::cout << MAJOR_VERSION << "." << MINOR_VERSION << "." << PATCH_VERSION << "\n";
 }
 
 #endif
