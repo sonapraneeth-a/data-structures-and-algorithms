cb433c1962ab1b6d779a9e575c36d6e5015ca0a9
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-07-07 14:13:10 +0530

    Updated source header files

--- Src/Headers/DefaultHeaders.h
+++ Src/Headers/DefaultHeaders.h
@@ -1,6 +1,6 @@
 /**
  *      Created on: 22 March 2018
- *   Last modified: 22 March 2018
+ *   Last modified: 04 July 2018
  *          Author: Sona Praneeth Akula
  *         Details: General Header file
  */
@@ -11,54 +11,15 @@
  * Date (DD-MM-YYYY)            Author               Update
  * 22-03-2018             Sona Praneeth Akula   - Creation of file
  *                                              - Added definitions for timing functions
+ * 04-07-2018             Sona Praneeth Akula   - Moved Exceptions header outside the Exceptions folder
  */
 
 #ifndef DEFAULT_HEADERS
 #define DEFAULT_HEADERS
 
-#include <iostream>
+#include "LibHeaders.h"
 #include "Utilities.h"
 #include "Exceptions.h"
-#include <ctime>
-#include <chrono>
 
-#define ULLI unsigned long long int
-#define LLI long long int
-
-#define MAJOR_VERSION 0
-#define MINOR_VERSION 3
-#define PATCH_VERSION 0
-
-
-#define EXECUTE_FUNC(func) \
-    {\
-        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
-        func\
-        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
-        std::cout << "Duration: " << duration << " us\n";\
-    }
-
-#define EXECUTE_FUNC_UNIT(unit, code_block) \
-    {\
-        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
-        code_block\
-        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
-        auto duration = 0;\
-        if (unit == "ns")\
-            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();\
-        else if (unit == "s")\
-            duration = std::chrono::duration_cast<std::chrono::seconds>(t2-t1).count();\
-        else if (unit == "ms")\
-            duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();\
-        else if (unit == "us")\
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
-        else\
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
-        if(!(unit == "ns" || unit == "ms" || unit == "us" || unit == "s"))\
-            std::cout << "Duration: " << duration << " us\n";\
-        else\
-            std::cout << "Duration: " << duration << " " << unit << "\n";\
-    }
 
 #endif
--- Src/Headers/Utilities.h
+++ Src/Headers/Utilities.h
@@ -5,13 +5,72 @@
 #include <ctime>
 #include "DefaultHeaders.h"
 
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 3
+#define PATCH_VERSION 0
+
+#define ULLI unsigned long long int
 #define LLI long long int
 
-int
-GetRandomInt(int start=100, int end=100000, std::string mode="Fixed");
+#define EXECUTE_FUNC(func) \
+    {\
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
+        func\
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
+        std::cout << "Duration: " << duration << " us\n";\
+    }
 
-double
-GetRandomDouble(int start=100, int end=100000, std::string mode="Fixed");
+#define EXECUTE_FUNC_UNIT(unit, code_block) \
+    {\
+        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
+        code_block\
+        std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
+        auto duration = 0;\
+        if (unit == "ns")\
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();\
+        else if (unit == "s")\
+            duration = std::chrono::duration_cast<std::chrono::seconds>(t2-t1).count();\
+        else if (unit == "ms")\
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();\
+        else if (unit == "us")\
+            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
+        else\
+            duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();\
+        if(!(unit == "ns" || unit == "ms" || unit == "us" || unit == "s"))\
+            std::cout << "Duration: " << duration << " us\n";\
+        else\
+            std::cout << "Duration: " << duration << " " << unit << "\n";\
+    }
+
+inline int
+GetRandomInt(int start=100, int end=100000, std::string mode="Fixed")
+{
+    int number;
+    if (mode == "Random")
+    {
+        srand((int) time(0));
+        number = (rand() % end) + start;
+    }
+    else
+    {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(start, end);
+        number = dist(mt);
+    }
+    return number;
+}
+
+inline double
+GetRandomDouble(int start=100, int end=100000, std::string mode="Fixed")
+{
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(start, end);
+    return dist(mt);
+}
+
 
 template <typename T>
 bool LessComparator(T first, T second)
@@ -23,6 +82,18 @@ template <typename T>
 bool GreaterComparator(T first, T second)
 {
     return first >= second;
+}
+
+template <typename T>
+bool StrictLessComparator(T first, T second)
+{
+    return first < second;
+}
+
+template <typename T>
+bool StrictGreaterComparator(T first, T second)
+{
+    return first > second;
 }
 
 template <typename T>
@@ -41,6 +112,12 @@ void PrintOneDArray(T Array[], LLI start, LLI end)
         std::cout << Array[index] << " ";
     }
     std::cout << "\n";
+}
+
+inline void
+PrintVersion()
+{
+    std::cout << MAJOR_VERSION << "." << MINOR_VERSION << "." << PATCH_VERSION << "\n";
 }
 
 #endif
