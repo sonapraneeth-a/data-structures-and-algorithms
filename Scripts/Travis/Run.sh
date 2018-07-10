7170bcc6ee6befde56ffcddc2a3c76f207b74f05
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-07-10 15:40:15 +0530

    Updated run script for travis

--- Scripts/Travis/Run.sh
+++ Scripts/Travis/Run.sh
@@ -4,13 +4,13 @@ set -x
 
 echo "Running data structures test"
 chmod +x ./Build/Test/DataStructuresTest
-./Build/Test/DataStructuresTest | tee -a "Test-DataStructures.txt"
+./Build/Test/DataStructuresTest #| tee -a "Test-DataStructures.txt"
 
 echo "Running algorithms test"
 chmod +x ./Build/Test/AlgorithmsTest
-./Build/Test/AlgorithmsTest | tee -a "Test-Algorithms.txt"
+./Build/Test/AlgorithmsTest #| tee -a "Test-Algorithms.txt"
 
-echo "Tests for 'Data structures'"
-tail -n 1 "Test-DataStructures.txt"
-echo "Tests for 'Algorithms'"
-tail -n 1 "Test-Algorithms.txt"
+#echo "Tests for 'Data structures'"
+#tail -n 1 "Test-DataStructures.txt"
+#echo "Tests for 'Algorithms'"
+#tail -n 1 "Test-Algorithms.txt"
