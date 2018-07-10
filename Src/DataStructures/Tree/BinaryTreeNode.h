8d9b1fc1605f02a68f763fac7aa95234e8667bb1
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-07-10 16:03:54 +0530

    Added starter code for BinaryTree, BinarySearchTree

--- Src/DataStructures/Tree/BinaryTreeNode.h
+++ Src/DataStructures/Tree/BinaryTreeNode.h
@@ -1,14 +1,43 @@
-//
-// Created by pranas on 3/20/2018.
-//
+/**
+ *      Created on: 20 March 2018
+ *   Last modified: 17 April 2018
+ *          Author: Sona Praneeth Akula
+ *         Details: Header for binary tree node
+ */
 
-#ifndef DATASTRUCTURESANDALGORITHMS_STACK_H
-#define DATASTRUCTURESANDALGORITHMS_STACK_H
+/**
+ * Changelog
+ *
+ * Date (DD-MM-YYYY)            Author               Update
+ * 20-03-2018             Sona Praneeth Akula   - Creation of the file
+ * 17-04-2018             Sona Praneeth Akula   - Added function signatures for binary tree node
+ */
 
+#ifndef BINARY_TREE_NODE_H
+#define BINARY_TREE_NODE_H
 
-class Stack {
+template <typename T>
+class BinaryTreeNode
+{
+private:
 
+    T _Data;
+    BinaryTreeNode<T>* _Left;
+    BinaryTreeNode<T>* _Right;
+
+public:
+
+    BinaryTreeNode(T Value);
+
+    BinaryTreeNode* GetLeftChild();
+    void SetLeftChild(BinaryTreeNode* Node);
+    BinaryTreeNode* GetRightChild();
+    void SetRightChild(BinaryTreeNode* Node);
+    T GetData();
+    void SetData(T Value);
+
+    ~BinaryTreeNode();
 };
 
 
-#endif //DATASTRUCTURESANDALGORITHMS_STACK_H
+#endif // BINARY_TREE_NODE_H
