5a86aeebf5f36bb2f8c5f7d56b4f3681cad32648
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-07-10 14:28:58 +0530

    Updated code for Math exponentation algorithms to use templated functions to allow integer data types

--- Src/Algorithms/Numbers/MainNumbers.cpp
+++ Src/Algorithms/Numbers/MainNumbers.cpp
@@ -1,6 +1,8 @@
 #include "DefaultHeaders.h"
 #include "BigInteger.h"
 #include "Factorization.h"
+#include "Math.h"
+#include "Utilities.h"
 
 void RunBigInteger()
 {
@@ -16,16 +18,16 @@ void RunGCD()
     LLI numOne = 1406700, numTwo = 164115;
     EXECUTE_FUNC_UNIT("ns",
                       std::cout << "GCD of " << numOne << " and " << numTwo << " is: "
-                                << NaiveGCD(numOne, numTwo) << "\n";)
+                                << Math::NaiveGCD(numOne, numTwo) << "\n";)
     EXECUTE_FUNC_UNIT("us",
                       std::cout << "GCD of " << numOne << " and " << numTwo << " is: "
-                                << EuclideanGCDRecursive(numOne, numTwo) << "\n";)
+                                << Math::EuclideanGCDRecursive(numOne, numTwo) << "\n";)
     return;
 }
 
 void RunPrimeFactorization()
 {
-    std::vector< std::pair<ULLI, ULLI> > factors = PrimeFactorization(100);
+    std::vector< std::pair<ULLI, ULLI> > factors = Math::PrimeFactorization(100);
     for(unsigned int index = 0; index < factors.size(); index++)
     {
         std::cout << "(" << factors[index].first << ", " << factors[index].second << ");";
@@ -33,8 +35,38 @@ void RunPrimeFactorization()
     std::cout << "\n";
 }
 
+void RunSieveOfEratosthenes()
+{
+    // std::vector< ULLI > test_cases{0, 1, 2, 3, 4, 5, 10, 1000, 1000000};
+    std::vector< ULLI > test_cases({1000000});
+    for(unsigned int test_case_index = 0; test_case_index < test_cases.size(); test_case_index++)
+    {
+        std::vector< ULLI > primes = Math::SieveOfEratosthenes(test_cases[test_case_index]);
+        std::cout << test_cases[test_case_index] << ": { ";
+        for(unsigned int factor_index = 0; factor_index < primes.size(); factor_index++)
+        {
+            std::cout << primes[factor_index] << ", ";
+        }
+        std::cout << "}\n";
+        primes.clear();
+    }
+    std::cout << "\n";
+}
+
+void RunExponentiation()
+{
+    std::cout << Math::ExponentiationRecursive(2, 3);
+}
+
 int main()
 {
-    RunBigInteger();
+    // RunSieveOfEratosthenes();
+    RunExponentiation();
+    /*int a = 5;
+    unsigned short int b = 5;
+    ULLI c = 78;
+    Math::typed_foo(a);
+    Math::typed_foo(b);
+    Math::typed_foo(c);*/
     return 0;
 }
