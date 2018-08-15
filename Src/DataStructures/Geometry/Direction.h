1dc47cc870c8f331ff1ac41e1810eb8d21077d00
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-08-15 22:03:10 +0530

    Added code Point and Direction in Geometry

---
+++ Src/DataStructures/Geometry/Direction.h
@@ -0,0 +1,17 @@
+//
+// Created by pranas on 7/15/2018.
+//
+
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+#include "VectorNd.h"
+#include "Vector3d.h"
+#include "Vector2d.h"
+
+typedef LinAlg::VectorNd DirectionNd;
+typedef LinAlg::Vector3d Direction3d;
+typedef LinAlg::Vector2d Direction2d;
+
+
+#endif //DIRECTION_H
