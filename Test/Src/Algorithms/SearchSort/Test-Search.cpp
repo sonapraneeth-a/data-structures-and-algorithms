50b0a246e58f85081fb0e4d0c5e1bb66598e7962
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-10-23 20:00:55 +0530

    Added minor tests for Search functions

--- Test/Src/Algorithms/SearchSort/Test-Search.cpp
+++ Test/Src/Algorithms/SearchSort/Test-Search.cpp
@@ -31,12 +31,13 @@ protected:
 
     }
 
-    int UnsortedArray_0[0] = {};
-    int UnsortedArray_1[1] = {1};
-    int UnsortedArray_2[2] = {2, 1};
-    int SortedArray_0[0] = {};
-    int SortedArray_1[1] = {1};
-    int SortedArray_2[2] = {1, 2};
+    int UnsortedArray_00[0] = {};
+    int UnsortedArray_01[1] = {1};
+    int UnsortedArray_02[2] = {2, 1};
+    int SortedArray_00[0] = {};
+    int SortedArray_01[1] = {1};
+    int SortedArray_02[2] = {1, 2};
+    int SortedArray_03[3] = {1, 2, 3};
 
 };
 
@@ -49,11 +50,11 @@ class BinarySearchRecursiveTest: public SearchTest
 class BinarySearchIterativeTest: public SearchTest
 {};
 
-TEST_F(LinearSearchTest, SmallArrayTest)
+TEST_F(LinearSearchTest, SmallArray_OutOfBounds_Test)
 {
     try
     {
-        LLI index = LinearSearch(UnsortedArray_1, 3, 0, 3);
+        LLI index = LinearSearch(UnsortedArray_01, 3, 0, 3);
         FAIL() << "Expected OutOfBoundsException()\n";
     }
     catch(OutOfBoundsException const & err)
@@ -64,21 +65,36 @@ TEST_F(LinearSearchTest, SmallArrayTest)
     {
         FAIL() << "Expected OutOfBoundsException()\n";
     }
-    EXPECT_EQ(0, LinearSearch(UnsortedArray_1, 1));
-    EXPECT_EQ(-1, LinearSearch(UnsortedArray_1, 3));
-    EXPECT_EQ(0, LinearSearch(UnsortedArray_2, 2));
 }
 
-TEST_F(BinarySearchRecursiveTest, SmallArrayTest)
+TEST_F(LinearSearchTest, SmallArray_Found_Test)
 {
-    // EXPECT_EQ(-1, BinarySearchRecursive(SortedArray_0, 0, 0, 0));
-    EXPECT_EQ(0, BinarySearchRecursive(SortedArray_1, 1, 0, 1));
-    EXPECT_EQ(1, BinarySearchRecursive(SortedArray_2, 2, 0, 2));
+    EXPECT_EQ(0, LinearSearch(UnsortedArray_01, 1));
+    EXPECT_EQ(0, LinearSearch(UnsortedArray_02, 2));
+}
+
+TEST_F(LinearSearchTest, SmallArray_NotFound_Test)
+{
+    EXPECT_EQ(-1, LinearSearch(UnsortedArray_01, 3));
+}
+
+TEST_F(BinarySearchRecursiveTest, SmallArray_Found_Test)
+{
+    // EXPECT_EQ(-1, BinarySearchRecursive(SortedArray_00, 0, 0, 0));
+    EXPECT_EQ(0, BinarySearchRecursive(SortedArray_01, 1, 0, 1));
+    EXPECT_EQ(1, BinarySearchRecursive(SortedArray_02, 2, 0, 2));
+}
+
+TEST_F(BinarySearchRecursiveTest, SmallArray_NotFound_Test)
+{
+    // EXPECT_EQ(-1, BinarySearchRecursive(SortedArray_00, 0, 0, 0));
+    EXPECT_EQ(0, BinarySearchRecursive(SortedArray_01, 1, 0, 1));
+    EXPECT_EQ(1, BinarySearchRecursive(SortedArray_02, 2, 0, 2));
 }
 
 TEST_F(BinarySearchIterativeTest, SmallArrayTest)
 {
-    // EXPECT_EQ(-1, BinarySearchIterative(SortedArray_0, 0, 0, 0));
-    EXPECT_EQ(0, BinarySearchIterative(SortedArray_1, 1, 0, 1));
-    EXPECT_EQ(1, BinarySearchIterative(SortedArray_2, 2, 0, 2));
+    // EXPECT_EQ(-1, BinarySearchIterative(SortedArray_00, 0, 0, 0));
+    EXPECT_EQ(0, BinarySearchIterative(SortedArray_01, 1, 0, 1));
+    EXPECT_EQ(1, BinarySearchIterative(SortedArray_02, 2, 0, 2));
 }
