f5404da721955873fb0f024146c7f9d642b9a96e
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-10-23 19:55:22 +0530

    Search header file updates
    1. Updated comments for Search header file
    2. Updated code according to Coding guidelines

--- Src/Algorithms/SearchSort/Search.h
+++ Src/Algorithms/SearchSort/Search.h
@@ -1,6 +1,6 @@
 /**
  *      Created on: 06 March 2018
- *   Last modified: 08 July 2018
+ *   Last modified: 24 August 2018
  *          Author: Sona Praneeth Akula
  *         Details: Header for algorithms related to searching array
  */
@@ -13,6 +13,9 @@
  * 06-05-2018             Sona Praneeth Akula   - Added function signatures for search algorithms
  *                                                linear and binary(iterative and recursive both)
  * 08-07-2018             Sona Praneeth Akula   - Moved implementations from .cpp file to .h file
+ * 23-08-2018             Sona Praneeth Akula   - Updated some comments
+ * 24-08-2018             Sona Praneeth Akula   - Updated some comments according to coding guidelines
+ *                                                document
  */
 
 #ifndef SEARCH_H
@@ -21,34 +24,36 @@
 #include "DefaultHeaders.h"
 
 /**
- * @brief  Searches for an element in the array in linear fashion
- * @details <b>Time Complexity:</b> O(N), where N is the elements to be searched for
+ * @brief  Searches for an element in the array in a linear fashion
+ * @details <b>Time Complexity:</b> O(N), where N is the length of the array
+ *          to be searched for<br/>
+ *          <b>Space Complexity:</b> O(1)
  *
  * @tparam T
- * @param [T] Array - Array in which the element has to be searched for
+ * @param [T] array - array in which the element has to be searched for
  * @param [T] element - Element to be searched in the array
  * @param [LLI - long long int] startIndex - From where to start searching in the array
  * @param [LLI- long long int] endIndex - Where to terminate the search
  * @return [LLI - long long int] index of the element in the array (0 indexing),
  *                                -1 if the element is not found in the array
  */
-template <typename T, LLI N>
-LLI LinearSearch(T (&Array)[N], T element, LLI startIndex = 0, LLI endIndex = -1)
+template <typename T, ULLI N>
+LLI LinearSearch(T (&array)[N], T element, LLI startIndex = 0, LLI endIndex = -1)
 {
-    ULLI lengthOfArray = SizeOfArray(Array);
-    if (lengthOfArray == 0)
+    ULLI __LengthOfArray = SizeOfArray(array);
+    if (__LengthOfArray == 0)
     {
         throw OutOfBoundsException();
     }
     if (endIndex == -1)
     {
-        endIndex = (LLI)lengthOfArray;
+        endIndex = (LLI)__LengthOfArray;
     }
-    if (!(startIndex >= 0 && startIndex <= lengthOfArray))
+    if (!(startIndex >= 0 && startIndex <= __LengthOfArray))
     {
         throw OutOfBoundsException();
     }
-    if (!(endIndex >= 0 && endIndex <= lengthOfArray))
+    if (!(endIndex >= 0 && endIndex <= __LengthOfArray))
     {
         throw OutOfBoundsException();
     }
@@ -60,7 +65,7 @@ LLI LinearSearch(T (&Array)[N], T element, LLI startIndex = 0, LLI endIndex = -1
     for (LLI index = startIndex; index < endIndex; ++index)
     {
         // Return the index if the element is found
-        if(Array[index] == element)
+        if(array[index] == element)
         {
             return index;
         }
@@ -70,10 +75,12 @@ LLI LinearSearch(T (&Array)[N], T element, LLI startIndex = 0, LLI endIndex = -1
 
 /**
  * @brief  Searches for an element in the array in binary search iterative version
- * @details <b>Time Complexity:</b> O(log N), where N is the elements to be searched for
+ * @details <b>Time Complexity:</b> O(log N), where N is the length of the array
+ *          to be searched for<br/>
+ *          <b>Space Complexity:</b> O(1)
  *
  * @tparam T
- * @param [T] Array - Array in which the element has to be searched for
+ * @param [T] array - array in which the element has to be searched for
  * @param [T] element - Element to be searched in the array
  * @param [LLI - long long int] startIndex - From where to start searching in the array
  * @param [LLI- long long int] endIndex - Where to terminate the search
@@ -81,19 +88,18 @@ LLI LinearSearch(T (&Array)[N], T element, LLI startIndex = 0, LLI endIndex = -1
  *                                -1 if the element is not found in the array
  */
 template <typename T, ULLI N>
-LLI BinarySearchIterative(T (&Array)[N], T element, LLI startIndex = 0, LLI endIndex = -1)
+LLI BinarySearchIterative(T (&array)[N], T element, LLI startIndex = 0, LLI endIndex = -1)
 {
-    ULLI lengthOfArray = SizeOfArray(Array);
-    // std::cout << "Length of array: " << lengthOfArray << "\n";
+    ULLI __LengthOfArray = SizeOfArray(array);
     if (endIndex == -1)
     {
-        endIndex = (LLI)lengthOfArray;
+        endIndex = (LLI)__LengthOfArray;
     }
-    if (!(startIndex >= 0 && startIndex <= lengthOfArray))
+    if (!(startIndex >= 0 && startIndex <= __LengthOfArray))
     {
         throw OutOfBoundsException();
     }
-    if (!(endIndex >= 0 && endIndex <= lengthOfArray))
+    if (!(endIndex >= 0 && endIndex <= __LengthOfArray))
     {
         throw OutOfBoundsException();
     }
@@ -106,12 +112,12 @@ LLI BinarySearchIterative(T (&Array)[N], T element, LLI startIndex = 0, LLI endI
     while (true)
     {
         // Get the index of the element in the middle of the array
-        LLI middleIndex = startIndex + (endIndex - startIndex)/2;
+        LLI __MiddleIndex = startIndex + (endIndex - startIndex)/2;
         // If the element required to be searched in the middle of the array
         // return the index
-        if(Array[middleIndex] == element)
+        if(array[__MiddleIndex] == element)
         {
-            return middleIndex;
+            return __MiddleIndex;
         }
         else if(startIndex > endIndex)
         {
@@ -119,15 +125,15 @@ LLI BinarySearchIterative(T (&Array)[N], T element, LLI startIndex = 0, LLI endI
         }
             // If the element we are looking for is greater than the middle element of the array, then
             // start searching in the right part of the array
-        else if(Array[middleIndex] < element)
+        else if(array[__MiddleIndex] < element)
         {
-            startIndex = middleIndex + 1;
+            startIndex = __MiddleIndex + 1;
         }
             // If the element we are looking for is lesser than the middle element of the array, then
             // start searching in the left part of the array
         else
         {
-            endIndex = middleIndex - 1;
+            endIndex = __MiddleIndex - 1;
         }
     }
     // If the lower index and higher index of the array have crossed each other and element
@@ -137,10 +143,12 @@ LLI BinarySearchIterative(T (&Array)[N], T element, LLI startIndex = 0, LLI endI
 
 /**
  * @brief  Searches for an element in the array in binary search recursive version
- * @details <b>Time Complexity:</b> O(log N), where N is the elements to be searched for
+ * @details <b>Time Complexity:</b> O(log N), where N is the length of the array
+ *          to be searched for<br/>
+ *          <b>Space Complexity:</b> O(1)
  *
  * @tparam T
- * @param [T] Array - Array in which the element has to be searched for
+ * @param [T] array - array in which the element has to be searched for
  * @param [T] element - Element to be searched in the array
  * @param [LLI - long long int] startIndex - From where to start searching in the array
  * @param [LLI- long long int] endIndex - Where to terminate the search
@@ -148,19 +156,19 @@ LLI BinarySearchIterative(T (&Array)[N], T element, LLI startIndex = 0, LLI endI
  *                                -1 if the element is not found in the array
  */
 template <typename T, ULLI N>
-LLI BinarySearchRecursive(T (&Array)[N], T element, LLI startIndex = 0, LLI endIndex = -1)
+LLI BinarySearchRecursive(T (&array)[N], T element, LLI startIndex = 0, LLI endIndex = -1)
 {
-    ULLI lengthOfArray = SizeOfArray(Array);
-    // std::cout << "Length of array: " << lengthOfArray << "\n";
+    ULLI __LengthOfArray = SizeOfArray(array);
+    // std::cout << "Length of array: " << __LengthOfArray << "\n";
     if (endIndex == -1)
     {
-        endIndex = (LLI)lengthOfArray;
+        endIndex = (LLI)__LengthOfArray;
     }
-    if (!(startIndex >= 0 && startIndex <= lengthOfArray))
+    if (!(startIndex >= 0 && startIndex <= __LengthOfArray))
     {
         throw OutOfBoundsException();
     }
-    if (!(endIndex >= 0 && endIndex <= lengthOfArray))
+    if (!(endIndex >= 0 && endIndex <= __LengthOfArray))
     {
         throw OutOfBoundsException();
     }
@@ -169,12 +177,12 @@ LLI BinarySearchRecursive(T (&Array)[N], T element, LLI startIndex = 0, LLI endI
         throw OutOfBoundsException();
     }
     // Get the index of the element in the middle of the array
-    LLI middleIndex = startIndex + (endIndex - startIndex)/2;
+    LLI __MiddleIndex = startIndex + (endIndex - startIndex)/2;
     // If the element required to be searched in the middle of the array
     // return the index
-    if(Array[middleIndex] == element)
+    if(array[__MiddleIndex] == element)
     {
-        return middleIndex;
+        return __MiddleIndex;
     }
         // If the lower index and higher index of the array have crossed each other and element
         // is still not found, then return -1
@@ -184,15 +192,15 @@ LLI BinarySearchRecursive(T (&Array)[N], T element, LLI startIndex = 0, LLI endI
     }
         // If the element we are looking for is greater than the middle element of the array, then
         // start searching in the right part of the array
-    else if(Array[middleIndex] < element)
+    else if(array[__MiddleIndex] < element)
     {
-        BinarySearchRecursive(Array, element, middleIndex + 1, endIndex);
+        BinarySearchRecursive(array, element, __MiddleIndex + 1, endIndex);
     }
         // If the element we are looking for is lesser than the middle element of the array, then
         // start searching in the left part of the array
     else
     {
-        BinarySearchRecursive(Array, element, startIndex, middleIndex - 1);
+        BinarySearchRecursive(array, element, startIndex, __MiddleIndex - 1);
     }
 }
 
