d0a430426a1e1f1e6ef3bce99244809acad69660
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-12-13 19:17:46 +0530

    Added Array module to the data structures project

---
+++ Src/DataStructures/Array/MainArray.cpp
@@ -0,0 +1,27 @@
+/**
+ * File description
+ *
+ *        Filename: MainArray.cpp
+ *      Created on: 13 December 2018
+ *   Last modified: 13 December 2018
+ *         Authors: sonapraneeth-a
+ *         Details: File for main executable of the project - array
+ */
+
+/**
+ * Changelog
+ *
+ * Date (DD-MM-YYYY)            Author               Update
+ * 13-12-2018               sonapraneeth-a      - Creation of the file
+ */
+
+
+#include "DefaultHeaders.h"
+
+int main()
+{
+    PrintProjectInfo();
+    std::cout << "\n";
+    std::cout << "Code for testing Array library\n";
+    return 0;
+}
