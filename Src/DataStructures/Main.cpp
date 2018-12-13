d8a6f8d365982106888233532ddec83776077cee
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-12-13 11:31:53 +0530

    Added basic code for data structures module

---
+++ Src/DataStructures/Main.cpp
@@ -0,0 +1,24 @@
+/**
+ * File description
+ *      Created on: 13 December 2018
+ *   Last modified: 13 December 2018
+ *         Authors: sonapraneeth-a
+ *         Details: File for main executable of the project - data structures
+ */
+
+/**
+ * Changelog
+ *
+ * Date (DD-MM-YYYY)            Author               Update
+ * 13-12-2018               sonapraneeth-a      - Creation of the file
+ *                                              - Printing simple string
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Data Structures Module\n";
+    return 0;
+}
+
