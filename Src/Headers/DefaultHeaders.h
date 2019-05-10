213ab16bb6df33b31b80501a6fcac1d3f9c00350
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2019-05-10 15:16:32 +0530

    Updated project info in default headers
    Signed-off-by: sonapraneeth-a <sonapraneeth.akula@gmail.com>

--- Src/Headers/DefaultHeaders.h
+++ Src/Headers/DefaultHeaders.h
@@ -27,13 +27,40 @@
 #include "StandardHeaders.h"
 #include "ProjectInfo.h"
 
-void
+
+ /*!
+  * @brief   Prints project related information on screen
+  * @details Details printed are name, project version, OS and compiler version
+  */
+void 
 PrintProjectInfo()
 {
     std::cout << "Project Details\n";
     std::cout << "========================================\n";
     std::cout << "   Name: " << __PROJECT_NAME__ << "\n";
     std::cout << "Version: " << __PROJECT_VERSION__ << "\n";
+    // Reference: https://blog.kowalczyk.info/article/j/guide-to-predefined-macros-in-c-compilers-gcc-clang-msvc-etc..html
+    std::cout << "OS Details\n";
+    std::cout << "----------------------------------------\n";
+#if defined(_WIN64) || defined(_WIN32)
+    std::cout << "Windows\n";
+#elif defined(__linux__)
+    std::cout << "Linux\n";
+#elif defined(__APPLE__)
+    std::cout << "Mac OS\n";
+#endif
+    std::cout << "Compiler Details\n";
+    std::cout << "----------------------------------------\n";
+#if defined (_MSC_VER)
+    // code specific to Visual Studio compiler
+    std::cout << "Compiler: Microsoft Visual C++ Compiler\n";
+    std::cout << " Version: " << _MSC_VER << _MSC_FULL_VER << "\n";
+#endif
+#if defined (__GNUC__)
+    // code specific to GNC GCC compiler
+    std::cout << "Compiler: GNU GCC Compiler\n";
+    std::cout << " Version: " << __GNUC__ << __GNUC_MINOR__ << __GNUC_PATCHLEVEL__ << "\n";
+#endif
 }
 
 void
