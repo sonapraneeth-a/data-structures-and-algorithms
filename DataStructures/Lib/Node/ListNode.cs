5c8103b8bd31e6f61a65b8d91331c1b7063b5c8f
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-06-08 00:45:48 +0530

    Added data structures project
    - Added node, main and export sub-projects
    
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

---
+++ DataStructures/Lib/Node/ListNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Node
+{
+    public class ListNode<T>
+    {
+        public T Data { get; set; }
+        public ListNode<T> Next { get; set; } = null;
+        public override string ToString()
+        {
+            if (Next == null)
+                return $"[{Data}] -> null";
+            return $"[{Data}] -> {Next?.GetHashCode()}";
+        }
+    }
+}
---
+++ DataStructures/Main/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Node;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode<int> Node = new ListNode<int>();
+            Node.Data = 10;
+            Console.WriteLine($"{Node}");
+        }
+    }
+}
