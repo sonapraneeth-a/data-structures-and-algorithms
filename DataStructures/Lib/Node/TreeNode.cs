bea98232a78ee8996edec6435915a1f8fa27bd06
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-06-12 23:38:13 +0530

    Added library and main code for TreeNode
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

---
+++ DataStructures/Lib/Node/TreeNode.cs
@@ -0,0 +1,85 @@
+﻿/**
+ * File description
+ *
+ *        Filename: TreeNode.cs
+ *      Created on: 08 June 2019
+ *   Last modified: 11 June 2019
+ *         Authors: sonapraneeth-a
+ *         Details: Node to be used in classes related to Trees
+ */
+
+/**
+ * Changelog
+ *
+ * Date (DD-MM-YYYY)            Author               Update
+ * 08-06-2019               sonapraneeth-a      - Creation of the file
+ * 11-06-2019               sonapraneeth-a      - Added comments to the code
+ *                                              - Added constructor for setting Data
+ */
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Node
+{
+    /// <summary>
+    /// Node class for <c>Tree</c> related classes
+    /// </summary>
+    public class TreeNode<T>
+    {
+        /// <summary>
+        /// <c>Data</c> property for the TreeNode
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// <c>Next</c> property for the TreeNode. This property contains information about
+        /// children of <c>TreeNode</c>
+        /// </summary>
+        public List<TreeNode<T>> Next { get; private set; } = null;
+
+        public TreeNode()
+        {
+            Data = default;
+        }
+        public TreeNode(T value)
+        {
+            Data = value;
+        }
+
+        /// <summary>
+        /// Adds a child to the current <c>TreeNode</c>
+        ///     <param><c>child</c> - Child to be added to the node</param>
+        /// </summary>
+        public void AddChild(TreeNode<T> child)
+        {
+            if (Next == null)
+            {
+                Next = new List<TreeNode<T>>();
+            }
+            Next.Add(child);
+        }
+
+        /// <summary>
+        /// Return the string representation of <c>TreeNode</c>
+        /// </summary>
+        public override string ToString()
+        {
+            // String representation when the node has no children
+            if (Next == null)
+            {
+                return $"[{Data}] -> null";
+            }
+            else
+            {
+                StringBuilder rep = new StringBuilder();
+                rep.AppendLine($" + [{Data}]");
+                foreach (TreeNode<T> item in Next)
+                {
+                    rep.AppendLine($" |-- {item.GetHashCode()} ");
+                }
+                return rep.ToString();
+            }
+        }
+    }
+}
---
+++ DataStructures/Main/TreeNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Node;
+
+namespace Main
+{
+    public static class TreeNodeMain
+    {
+        public static void Constructor()
+        {
+            Console.WriteLine("=== TreeNode Constructors ===");
+
+            Console.WriteLine("--- TreeNode Constructor(1) ---");
+            TreeNode<int> TNode_c1_01 = new TreeNode<int>();
+            TreeNode<int> TNode_c1_02 = new TreeNode<int>();
+            TreeNode<int> TNode_c1_03 = new TreeNode<int>();
+            TNode_c1_01.Data = 20;
+            TNode_c1_01.AddChild(TNode_c1_02);
+            // TNode_c1_01.AddChild(null);
+            TNode_c1_01.AddChild(TNode_c1_03);
+            TNode_c1_02.Data = 30;
+            Console.WriteLine($"TNode_c1_01: {TNode_c1_01}");
+
+            Console.WriteLine("--- TreeNode Constructor(2) ---");
+            TreeNode<int> TNode_c2_01 = new TreeNode<int>(10);
+            TreeNode<int> TNode_c2_02 = new TreeNode<int>(20);
+            TreeNode<int> TNode_c2_03 = new TreeNode<int>(30);
+            TNode_c2_01.AddChild(TNode_c2_02);
+            // TNode_c2_01.AddChild(null);
+            TNode_c2_01.AddChild(TNode_c2_03);
+            Console.WriteLine($"TNode_c2_01: {TNode_c2_01}");
+
+            Console.WriteLine();
+        }
+    }
+}
