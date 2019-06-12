812ef02431f5b7dd63264237ca3efaa5e2b05d3a
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-06-12 23:37:08 +0530

    Updated library and main code for ListNode
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- DataStructures/Lib/Node/ListNode.cs
+++ DataStructures/Lib/Node/ListNode.cs
@@ -1,11 +1,19 @@
-﻿using System;
-
-namespace Node
+﻿namespace Node
 {
     public class ListNode<T>
     {
         public T Data { get; set; }
         public ListNode<T> Next { get; set; } = null;
+        public ListNode()
+        {
+            // Set default value in case no argument is supplied to the 
+            // constructor
+            Data = default;
+        }
+        public ListNode(T value)
+        {
+            Data = value;
+        }
         public override string ToString()
         {
             if (Next == null)
---
+++ DataStructures/Main/ListNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Node;
+
+namespace Main
+{
+    public static class NodeMain
+    {
+        public static void Constructor()
+        {
+            Console.WriteLine("=== Node Constructors ===");
+
+            Console.WriteLine("--- Node Constructor(1) ---");
+            ListNode<int> Node_c1_01 = new ListNode<int>();
+            Console.WriteLine($"Node_c1_01 : {Node_c1_01}");
+
+            Console.WriteLine("--- Node Constructor(2) ---");
+            ListNode<int> Node_c2_02 = new ListNode<int>(10);
+            Console.WriteLine($"Node_c2_02 : {Node_c2_02}");
+
+            Console.WriteLine();
+        }
+    }
+}
