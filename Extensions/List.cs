298c70db5f9e8e5f4bbe1bd70d57d5c7145ef8da
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-06-17 00:18:49 +0530

    Added extensions project extension methods for list
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

---
+++ Extensions/List.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public static class ListExtension
+    {
+        public static string StringRepresentation<T>(this List<T> list)
+        {
+            string rep = "{ ";
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (index != list.Count - 1)
+                    rep += $"{list[index]}, ";
+                else
+                    rep += $"{list[index]} ";
+            }
+            rep += "}";
+            return rep;
+        }
+    }
+}
